01e6d1a6a1535fc0dc3b1e0eb48ef271c4457567
Simonov Anatoliy <tolylya220@gmail.com>
2017-08-22 14:48:58 +0300

    Helpers test

---
+++ src/utils/articleHelper.spec.ts
@@ -0,0 +1,65 @@
+import { getAuthors, getComments, normalizeArticles } from './articlesHelper';
+// import 'jest';
+// import 'mocha';
+// import {} from 'mocha';
+// import {} from 'jasmine-light'
+
+describe('Helpers::Article', () => {
+  const articles = [{
+    'id': '1',
+    'author': {
+      'id': '1',
+      'name': 'Polly Jane'
+    },
+    'title': 'How I spent my summer',
+    'text': 'Few words about my summer story...',
+    'comments': [{
+      'id': '1',
+      'text': 'Paris is so cool!',
+      'commenter': {
+        'id': '2',
+        'name': 'John Galt'
+      }
+    }]
+  }];
+
+  it('should return normalized articles', () => {
+    const expected = [{
+      'id': '1',
+      'author':'1',
+      'title': 'How I spent my summer',
+      'text': 'Few words about my summer story...',
+      'comments': [{
+        'id': '1',
+        'text': 'Paris is so cool!',
+        'commenter': '2'
+      }
+      ]
+    }];
+
+    expect(normalizeArticles(articles)).toEqual(expected);
+  });
+
+  it('should return all authors of article', () => {
+    const expected = [{
+      'id': '1',
+      'name': 'Polly Jane'
+    }, {
+      'id': '2',
+      'name': 'John Galt'
+    }];
+
+    expect(getAuthors(articles)).toEqual(expected);
+  });
+
+  it('should return comments of article', () => {
+    const expected = [{
+      'id': '1',
+      'text': 'Paris is so cool!',
+      'commenter': '2'
+    }];
+
+    expect(getComments(articles)).toEqual(expected);
+  });
+
+});
