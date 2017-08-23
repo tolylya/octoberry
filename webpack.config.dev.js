1391dbd11a0d248403f2f0bc696338b92d84c022
Simonov Anatoliy <tolylya220@gmail.com>
2017-08-23 09:03:15 +0300

    Tslint included

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -59,7 +59,8 @@ export default {
       {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
       {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
       {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']},
-      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'}
+      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
+      {test: /\.tsx?$/, enforce: 'pre', loader: 'tslint-loader'}
     ]
   }
 };
